f0d122dec93ae296ae775d232a1a3ad307575548
Thomas Nicklin <tnicklin@uber.com>
2026-02-08 17:51:15 -0700

    [keydb] simplify wcl config

--- warcraftlogs/config.go
+++ warcraftlogs/config.go
@@ -4,11 +4,4 @@ package warcraftlogs
 type Config struct {
 	ClientID     string `yaml:"client_id"`
 	ClientSecret string `yaml:"client_secret"`
-	GraphQLURL   string `yaml:"graphql_url"`
-	TokenURL     string `yaml:"token_url"`
-	UserAgent    string `yaml:"user_agent"`
-	GuildName    string `yaml:"guild_name"`
-	ServerSlug   string `yaml:"server_slug"`
-	ServerRegion string `yaml:"server_region"`
-	Limit        int    `yaml:"limit"`
 }
